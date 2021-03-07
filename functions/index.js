0ba58b6109b03de649b9de361361f9c7ccd2696a
Sheena Cheng <chengsheena25@gmail.com>
2021-03-06 18:11:35 -0700

    Working on Vision API

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,57 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const admin = require('firebase-admin');
+const vision = require('@google-cloud/vision');
+const visionClient =  new vision.ImageAnnotatorClient();
+let Promise = require('promise');
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+
+exports.analyzeImage = functions.firestore.document('photos/{document}').onCreate((snap, context) => {
+
+	console.log('SNAP', snap)
+	console.log('CONTEXT', context)
+
+	const data = snap.data();
+	console.log('DATA IN IS', data)
+	const photoUrl = "gs://" + data.bucket + "/" + data.fullPath;
+	console.log('url is', photoUrl);
+
+	return Promise.resolve()
+	.then(() => {
+		return visionClient.labelDetection(photoUrl);
+	})
+	.then(results => {
+		console.log('VISION data all is: ', results)
+		
+		/*
+		const labels = results.labelAnnotations;
+		console.log('Labels: ');
+		labels.forEach(label => console.log(label.description));
+		*/
+	})
+	.catch(err => console.log(err));
+
+	/*
+	const [result] = await client.objectLocalization(gcsUri);
+	const objects = result.localizedObjectAnnotations;
+
+	objects.forEach(object => {
+	  console.log(`Name: ${object.name}`);
+	  console.log(`Confidence: ${object.score}`);
+	  const veritices = object.boundingPoly.normalizedVertices;
+	  veritices.forEach(v => console.log(`x: ${v.x}, y:${v.y}`));
+	});
+	*/
+	
+	/*
+	  // Performs label detection on the image file
+	  const [result] = await client.labelDetection('./resources/wakeupcat.jpg');
+	  const labels = result.labelAnnotations;
+	  console.log('Labels:');
+	  labels.forEach(label => console.log(label.description));
+	*/
+
+})

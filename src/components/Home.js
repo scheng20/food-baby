f4c7930f9fcc6d8a7ba7c5c4f552a1bce80da39e
jasmineke <jasmine.ke99@gmail.com>
2021-03-07 03:32:31 -0700

    almost done home, need to do menu still

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,19 +12,24 @@ class Home extends React.Component {
 
                 <h1>Hi there, <br/> welcome to food-baby</h1>
                 <div className="bodyStart">
-                    <div className="col-60">
                         <div className="homeImage">
                         <img src = '../graphics/home1.png' alt=""/>
                         </div>
                 <p className="bodyStyle">1. Search for your food by taking a picture or typing it in!</p>
-                <p className="bodyStyle">2. Select the foods you’re craving, we’ll keep track.</p>
-                <p className="bodyStyle">3. Vuala! We’ll show you which ones are good and bad, click into them to find out more!</p>
+                    <div className="homeImage1">
+                        <img src = '../graphics/home2.png' alt=""/>
                     </div>
+                <p className="bodyStyle">2. Select the foods you’re craving, we’ll keep track.</p>
+                    <div className="homeImage2">
+                        <img src = '../graphics/home2.png' alt=""/>
+                    </div>
+                <p className="bodyStyle">3. Vuala! We’ll show you which ones are good and bad, click into them to find out more!</p>
+
                 </div>
                 <div className="leftUpAdjust">
-                    <button1><a href="/search">Search</a></button1>
-                    <button2><a href="/upload">Upload</a></button2>
-                    <button3><a href="/camera">Camera</a></button3>
+                    <button1 className = "color"><a href="/search">Search</a></button1>
+                    <button2 className = "color"><a href="/upload">Upload</a></button2>
+                    <button3 className = "color"><a href="/camera">Camera</a></button3>
                     <rect className="bottomBar"></rect>
                 </div>
             </div></div>

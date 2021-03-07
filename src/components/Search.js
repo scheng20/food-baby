099cc85c14e9f7a7dac807b96dc4ef8964e4fd93
Tiantian Li <tiantianli007@yahoo.com>
2021-03-06 21:11:17 -0500

    a

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,38 +1,98 @@
 import React from "react";
 
-const Search = () => {
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          itemList: [],
+          item: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
 
-    const getInitialState = () => {			
-		return { toByGroceryItems : [], purchasedGroceryItems : [] }
+
+    handleChange(event) {
+        this.setState({item: event.target.value});
+    }
+
+    handleSubmit(event) {
+        //alert('An essay was submitted: ' + this.state.item);
+        //this.state.itemList.push(this.state.item);
+        this.setState({ ...this.state, itemList: this.state.itemList.concat([this.state.item]) });
+        event.preventDefault();
     }
     
-    const handleGroceryItemAddition = (groceryItem) => {	
-		var allItems = this.state.toByGroceryItems.concat([groceryItem]);
-		this.setState({toByGroceryItems: allItems});				
-	}
-
-    // handleChange = (event) => {
-    //     this.setState({item: event.target.value});
+    // addItem(e) {
+    //     if (this.state.item !== "") {
+    //       var newItem = {
+    //         item: this._inputElement.value,
+    //         key: Date.now()
+    //       };
+       
+    //       this.setState((prevState) => {
+    //         return { 
+    //           items: prevState.items.concat(newItem) 
+    //         };
+    //       });
+         
+    //       this._inputElement.value = "";
+    //     }
+         
+    //     console.log(this.state.items);
+           
+    //     e.preventDefault();
     // }
 
-    // handleSubmit = () => {
-    //     this.props.list.push(this.state.item);
-    //     alert(this.props.list);
-    //     //event.preventDefault();
-    // }
+    render() {
+        console.log(this.state);
+        return (
+            <div>
+                <h1>Search</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange}></input>
+                    <input type="submit" value="Confirm"></input>
+                </form>
 
-
-    return (
-        <div>
-            <h1>Search</h1>
-            {/* <GroceryItemAddForm onFormSubmit={this.handleGroceryItemAddition}/> */}
-                {/* <input type="text" name={this.state.item} onChange={this.handleChange}></input>
-                <button onClick={this.handleSubmit}>Search</button> */}
-
-        </div>
-    );
-
+                <button>Confirm</button>
+            </div>
+        );
+    }
 }
+
+// const Search = () => {
+
+//     const getInitialState = () => {			
+// 		return { toByGroceryItems : [], purchasedGroceryItems : [] }
+//     }
+    
+//     const handleGroceryItemAddition = (groceryItem) => {	
+// 		var allItems = this.state.toByGroceryItems.concat([groceryItem]);
+// 		this.setState({toByGroceryItems: allItems});
+//     }
+
+//     // handleChange = (event) => {
+//     //     this.setState({item: event.target.value});
+//     // }
+
+//     // handleSubmit = () => {
+//     //     this.props.list.push(this.state.item);
+//     //     alert(this.props.list);
+//     //     //event.preventDefault();
+//     // }
+
+
+//     return (
+//         <div>
+//             <h1>Search</h1>
+//             <GroceryItemAddForm onFormSubmit={this.handleGroceryItemAddition}/>
+//                 {/* <input type="text" name={this.state.item} onChange={this.handleChange}></input>
+//                 <button onClick={this.handleSubmit}>Search</button> */}
+
+//         </div>
+//     );
+
+// }
 
 // var GroceryItemAddForm = React.createClass({
 // 	getInitialState : function() {
